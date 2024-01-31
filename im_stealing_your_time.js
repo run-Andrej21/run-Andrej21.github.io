bafff129b1790751f53fc46807050949b8ae76d9
run-Nexus21 <run-Nexus21@gmail.com>
2024-01-31 09:11:04 +0100

    delete "im_stealing_your_time.js"

--- im_stealing_your_time.js
+++ im_stealing_your_time.js
@@ -1,9 +1,1 @@
-let time = document.getElementById("HowTFDoIMakeTime");
-
-        setInterval(() =>{
-            
-            let d = new Date();
-            time.innerText = d.toLocaleTimeString([], {hour: "2-digit", minute: "2-digit", second: "2-digit", hour12: false});
-            
-        });
 
--- the-cookie.js
+++ the-cookie.js
@@ -49,3 +49,14 @@ function setCookie(cname, cvalue, exdays) {
       
 
     });
+
+//Time
+
+    let time = document.getElementById("HowTFDoIMakeTime");
+
+        setInterval(() =>{
+            
+            let d = new Date();
+            time.innerText = d.toLocaleTimeString([], {hour: "2-digit", minute: "2-digit", second: "2-digit", hour12: false});
+            
+        });
