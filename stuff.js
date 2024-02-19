e305842dac394f324d3b8f268b350d09173c39af
run-Nexus21 <run-Nexus21@gmail.com>
2024-02-19 15:57:41 +0100

    way too much bs

--- stuff.js
+++ stuff.js
@@ -1,21 +1,104 @@
- //Taskbar start
- //settings
-  var settings_overlay = document.querySelector(".settings_check");
-  var settings = document.getElementById("open_settings_id");
-//visuals
-  var visuals_overlay = document.querySelector(".visuals_check");
-  var visuals = document.getElementById("open_visuals_id");
-//settings //shortcut
-  var settings_shortcut_overlay = document.querySelector(".settings_shortcut_check");
-  var settings_shortcut = document.getElementById("settings_shortcut_id");
 
-  //Visuals popup
-document.getElementById("open_visuals_id").addEventListener("click", function() {
+
+
+
+function setCookie(cname, cvalue, exdays){
+
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+
+}
+
+function getCookie(cname){
+
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+
+    for(let i = 0; i <ca.length; i++){
+
+      let c = ca[i];
+
+      while(c.charAt(0) == ' '){
+
+        c = c.substring(1);
+
+      }
+
+      if(c.indexOf(name) == 0){
+
+        return c.substring(name.length, c.length);
+
+      }
+    }
+
+    return "";
+
+}
+
+document.addEventListener("DOMContentLoaded", function (){
+
+  var container = document.querySelector(".container");
+  let username = getCookie("username");
+
+  if(username != "") {
+    
+    container.style.display = "";
+    document.getElementById("textname").textContent = ("Hello, " + username);
+
+  }else{
+
+    var showFS = document.querySelector(".FS");
+    showFS.style.display ="";     
+      
+  }
+});
+
+document.getElementById("del_name_id").addEventListener("click", function(){
+
+  location.reload();
+  document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+  
+});
+
+//Time
+
+let time = document.getElementById("HowTFDoIMakeTime_id");
+
+setInterval(() =>{
+      
+let d = new Date();
+time.innerText = d.toLocaleTimeString([], {hour: "2-digit", minute: "2-digit", second: "2-digit", hour12: false});
+      
+});
+
+//Taskbar start
+
+//settings
+
+var settings_overlay = document.querySelector(".settings_check");
+var settings = document.getElementById("open_settings_id");
+
+//visuals
+
+var visuals_overlay = document.querySelector(".visuals_check");
+var visuals = document.getElementById("open_visuals_id");
+
+//settings //shortcut
+
+var settings_shortcut_overlay = document.querySelector(".settings_shortcut_check");
+var settings_shortcut = document.getElementById("settings_shortcut_id");
+
+//Visuals popup
+
+document.getElementById("open_visuals_id").addEventListener("click", function(){
 
   if (! visuals.classList.contains("opened")) {
     
     
-    settings_overlay.style.display = "none"
+    settings_overlay.style.display = "none";
     settings.classList.remove("opened");
 
     settings_shortcut_overlay.style.display = "none";
@@ -24,7 +107,7 @@ document.getElementById("open_visuals_id").addEventListener("click", function() 
     visuals_overlay.style.display = "block";
     visuals.classList.add("opened");
 
-  } else {
+  }else{
 
     visuals_overlay.style.display = "none";
     visuals.classList.remove("opened");
@@ -34,46 +117,41 @@ document.getElementById("open_visuals_id").addEventListener("click", function() 
 
 //visuals stuff
 
- document.getElementById("change_background_id").addEventListener("click", function(){
+document.getElementById("change_background_id").addEventListener("click", function(){
 
- document.getElementById("test1_id_id").click()
+  var visuals_background_overlay = document.querySelector(".visuals_background_check")
+  var visuals_the_background =document.getElementById("visuals_the_background_id")
 
- document.getElementById("test1_id_id").addEventListener("click", function(event){
- 
-
-  var background = document.querySelector(".container");
-  var what_background = document.getElementById("test1_id_id").value;
-
-  addEventListener("")
-  console.log(what_background)
-
- })
- })
-
-  //Settings popup
-document.getElementById("open_settings_id").addEventListener("click", function() {
-
-    if (! settings.classList.contains("opened")) {
-
-      visuals_overlay.style.display = "none";
-      visuals.classList.remove("opened");
-
-      settings_overlay.style.display = "block";
-      settings.classList.add("opened");
-
-      settings_shortcut_overlay.style.display = "none";
-      settings_shortcut.classList.remove("opened");
-
-    } else {
-
-      settings.classList.remove("opened");
-      settings_overlay.style.display = "none";
-
-      settings_shortcut_overlay.style.display = "none";
-      settings_shortcut.classList.remove("opened");
-
-    }
+  if(! visuals_the_background.classList.contains("opened")){
+    
+  }
 });
+//Settings popup
+
+document.getElementById("open_settings_id").addEventListener("click", function(){
+
+  if(! settings.classList.contains("opened")){
+
+    visuals_overlay.style.display = "none";
+    visuals.classList.remove("opened");
+
+    settings_overlay.style.display = "block";
+    settings.classList.add("opened");
+
+    settings_shortcut_overlay.style.display = "none";
+    settings_shortcut.classList.remove("opened");
+
+  }else{
+
+    settings.classList.remove("opened");
+    settings_overlay.style.display = "none";
+
+    settings_shortcut_overlay.style.display = "none";
+    settings_shortcut.classList.remove("opened");
+
+  }
+});
+
 //Taskbar end
 
 //settings //Shortcut Settings
@@ -81,14 +159,14 @@ document.getElementById("open_settings_id").addEventListener("click", function()
 var settings_shortcut_overlay = document.querySelector(".settings_shortcut_check");
 var settings_shortcut = document.getElementById("settings_shortcut_id");
 
-document.getElementById("shortcut_button_id").addEventListener("click", function() {
+document.getElementById("shortcut_button_id").addEventListener("click", function(){
 
   if(! settings_shortcut.classList.contains("opened")) {
 
     settings_shortcut_overlay.style.display = "block";
     settings_shortcut.classList.add("opened");
 
-  } else {
+  }else{
 
     settings_shortcut_overlay.style.display = "none";
     settings_shortcut.classList.remove("opened");
@@ -96,141 +174,119 @@ document.getElementById("shortcut_button_id").addEventListener("click", function
 })
 
 //shortcut #1
-    //from inout into a var
+  //from inout into a var
 
-    document.getElementById("shortcut1_input").addEventListener("keypress", function(shortcut1) {
+document.getElementById("shortcut1_input").addEventListener("keypress", function(shortcut1){
 
-      if (shortcut1.key === "Enter") {
-  
-        var shortcut1_stuff = document.getElementById("shortcut1_input").value;
+  if(shortcut1.key === "Enter"){
 
-        let cname = shortcut1_stuff;
+    var shortcut1_stuff = document.getElementById("shortcut1_input").value;
+    let cname = shortcut1_stuff;
+    setCookie("shortcut_1_stuff", cname, 365);
 
-        setCookie("shortcut_1_stuff", cname, 365);
+  }
+})
 
-      }})
-  //if button
+//if button
 
-        document.getElementById("shortcut1_id").addEventListener("click", function() {
+document.getElementById("shortcut1_id").addEventListener("click", function(){
 
-          let shortcut_1_cookie = getCookie("shortcut_1_stuff");
+  let shortcut_1_cookie = getCookie("shortcut_1_stuff");
+  window.location = shortcut_1_cookie;
 
-          window.location = shortcut_1_cookie
-  
-          }
-        )
-
+})
 
 //shortcut #2
-    //from inout into a var
+  //from inout into a var
 
-    document.getElementById("shortcut2_input").addEventListener("keypress", function(shortcut2) {
+document.getElementById("shortcut2_input").addEventListener("keypress", function(shortcut2){
 
-      if (shortcut2.key === "Enter") {
-  
-        var shortcut2_stuff = document.getElementById("shortcut2_input").value;
-  
-        console.log(shortcut2_stuff)
+  if(shortcut2.key === "Enter"){
 
-        let cname = shortcut2_stuff;
+    var shortcut2_stuff = document.getElementById("shortcut2_input").value;
+    let cname = shortcut2_stuff;
+    setCookie("shortcut_2_stuff", cname, 365);
 
-        setCookie("shortcut_2_stuff", cname, 365);
+  }
+})
 
-      }})
-  //if button
+//if button
 
-        document.getElementById("shortcut2_id").addEventListener("click", function() {
+document.getElementById("shortcut2_id").addEventListener("click", function(){
 
-          let shortcut_2_cookie = getCookie("shortcut_2_stuff");
+  let shortcut_2_cookie = getCookie("shortcut_2_stuff");
+  window.location = shortcut_2_cookie;
 
-          window.location = shortcut_2_cookie
-  
-          }
-        )
-
-        
+})
+      
 //shortcut #3
-    //from inout into a var
+  //from inout into a var
 
-    document.getElementById("shortcut3_input").addEventListener("keypress", function(shortcut3) {
+document.getElementById("shortcut3_input").addEventListener("keypress", function(shortcut3){
 
-      if (shortcut3.key === "Enter") {
-  
-        var shortcut3_stuff = document.getElementById("shortcut3_input").value;
-  
-        console.log(shortcut3_stuff)
+  if (shortcut3.key === "Enter"){
 
-        let cname = shortcut3_stuff;
+    var shortcut3_stuff = document.getElementById("shortcut3_input").value;
+    let cname = shortcut3_stuff;
+    setCookie("shortcut_3_stuff", cname, 365);
 
-        setCookie("shortcut_3_stuff", cname, 365);
+  }
+})
 
-      }})
-  //if button
+//if button
 
-        document.getElementById("shortcut3_id").addEventListener("click", function() {
+document.getElementById("shortcut3_id").addEventListener("click", function(){
 
-          let shortcut_3_cookie = getCookie("shortcut_3_stuff");
+  let shortcut_3_cookie = getCookie("shortcut_3_stuff");
+  window.location = shortcut_3_cookie;
 
-          window.location = shortcut_3_cookie
-  
-          }
-        )
-
-        
+});
+      
 //shortcut #4
-    //from inout into a var
+  //from inout into a var
 
-    document.getElementById("shortcut4_input").addEventListener("keypress", function(shortcut4) {
+document.getElementById("shortcut4_input").addEventListener("keypress", function(shortcut4){
 
-      if (shortcut4.key === "Enter") {
-  
-        var shortcut4_stuff = document.getElementById("shortcut4_input").value;
-  
-        console.log(shortcut4_stuff)
+  if(shortcut4.key === "Enter"){
 
-        let cname = shortcut4_stuff;
+    var shortcut4_stuff = document.getElementById("shortcut4_input").value;
+    let cname = shortcut4_stuff;
+    setCookie("shortcut_4_stuff", cname, 365);
 
-        setCookie("shortcut_4_stuff", cname, 365);
+  }
+});
 
-      }})
-  //if button
+//if button
 
-        document.getElementById("shortcut4_id").addEventListener("click", function() {
+document.getElementById("shortcut4_id").addEventListener("click", function(){
 
-          let shortcut_4_cookie = getCookie("shortcut_4_stuff");
+  let shortcut_4_cookie = getCookie("shortcut_4_stuff");
+  window.location = shortcut_4_cookie;
 
-          window.location = shortcut_4_cookie
-  
-          }
-        )
+});
 
-        //shortcut #5
-    //from inout into a var
+      //shortcut #5
+  //from inout into a var
 
-    document.getElementById("shortcut5_input").addEventListener("keypress", function(shortcut5) {
+document.getElementById("shortcut5_input").addEventListener("keypress", function(shortcut5){
 
-      if (shortcut5.key === "Enter") {
-  
-        var shortcut5_stuff = document.getElementById("shortcut5_input").value;
-  
-        console.log(shortcut5_stuff)
+  if(shortcut5.key === "Enter"){
 
-        let cname = shortcut5_stuff;
+    var shortcut5_stuff = document.getElementById("shortcut5_input").value;
+    let cname = shortcut5_stuff;
+    setCookie("shortcut_5_stuff", cname, 365);
 
-        setCookie("shortcut_5_stuff", cname, 365);
+  }
+});
 
-      }})
-  //if button
+//if button
 
-        document.getElementById("shortcut5_id").addEventListener("click", function() {
+document.getElementById("shortcut5_id").addEventListener("click", function(){
 
-          let shortcut_5_cookie = getCookie("shortcut_5_stuff");
+  let shortcut_5_cookie = getCookie("shortcut_5_stuff");
+  window.location = shortcut_5_cookie;
 
-          window.location = shortcut_5_cookie
-  
-          }
-        )
-
+});
 
 //codefield
 
@@ -243,51 +299,50 @@ var codefield2_overlay = document.querySelector(".codefield2_text_check")
 var codefield2_id = document.getElementById("codefield2_text_id")
 let CF_ok = false;
 
-document.addEventListener("keydown", function (down) {
+document.addEventListener("keydown", function (down){
 
-if (down.code === CF_code[CF_pos]) {
+  if(down.code === CF_code[CF_pos]){
 
-  CF_pos++
-  console.log(down)
+    CF_pos++
 
-} else {
+  }else{
 
-  CF_pos = 0;
+    CF_pos = 0;
 
-}
+  }
 
-if (CF_pos === 4) {
+  if(CF_pos === 4){
 
-  stopthisshit = !stopthisshit
-  CF_ok = true;
-  
-}
+    stopthisshit = !stopthisshit
+    CF_ok = true;
 
-if (CF_ok === true) {
+  }
 
-if (stopthisshit === true) {
+    if(CF_ok === true){
 
-  codefield_id.classList.add("opened");
-  codefield_overlay.style.display = "block"
-  
-  codefield2_id.classList.add("opened");
-  codefield2_overlay.style.display = "block"
+    if(stopthisshit === true){
 
-  CF_pos = 0;
-  CF_ok = false;
+      codefield_id.classList.add("opened");
+      codefield_overlay.style.display = "block";
 
-} else {
+      codefield2_id.classList.add("opened");
+      codefield2_overlay.style.display = "block";
 
-  codefield_id.classList.remove("opened");
-  codefield_overlay.style.display = "none";
+      CF_pos = 0;
+      CF_ok = false;
 
-  codefield2_id.classList.remove("opened");
-  codefield2_overlay.style.display = "none";
+    }else{
 
-  CF_pos = 0;
-  CF_ok = false;
+      codefield_id.classList.remove("opened");
+      codefield_overlay.style.display = "none";
 
-}
+      codefield2_id.classList.remove("opened");
+      codefield2_overlay.style.display = "none";
+
+      CF_pos = 0;
+      CF_ok = false;
+
+    }
   }
 });
 
@@ -295,58 +350,97 @@ if (stopthisshit === true) {
 
 document.addEventListener('DOMContentLoaded', function () {
 
-  const textField1 = document.getElementById("codefield_text_id");
-  const textField2 = document.getElementById("codefield2_text_id");
+const textField1 = document.getElementById("codefield_text_id");
+const textField2 = document.getElementById("codefield2_text_id");
 
-  function Text(event) {
-      const newValue = event.target.value;
+function Text(event){
+  const newValue = event.target.value;
 
-      if (newValue === "21") {
-          
-          alert("sheeesh");
-
-          event.target.value = "";
-
-      }
-
-      if (newValue === "ms") {
-
-        var FS = document.querySelector(".FS");
-        var container = document.querySelector(".container")
+  if(newValue === "21"){
       
-        container.style.display = ""
-        FS.style.display = "none";
-
-        event.target.value = "";
-      
-      }
-
-      if (newValue === "fs") {
-
-        var FS = document.querySelector(".FS");
-        var container = document.querySelector(".container")
-      
-        container.style.display = "none"
-        FS.style.display = "";
-
-        event.target.value = "";
-      
-      }
-
-      if (newValue === "fulls") {
-
-        var elem = document.documentElement;
-      
-        elem.requestFullscreen();
-
-      }
+    alert("sheeesh");
+    event.target.value = "";
 
   }
 
-  textField1.addEventListener("keypress", Text);
-  textField2.addEventListener("keypress", Text);
+  if(newValue === "ms"){
+
+    var FS = document.querySelector(".FS");
+    var container = document.querySelector(".container");
+  
+    container.style.display = "";
+    FS.style.display = "none";
+    event.target.value = "";
+  
+  }
+
+  if(newValue === "fs"){
+
+    var FS = document.querySelector(".FS");
+    var container = document.querySelector(".container");
+  
+    container.style.display = "none";
+    FS.style.display = "";
+    event.target.value = "";
+  
+  }
+
+  if(newValue === "fulls"){
+
+    var elem = document.documentElement;
+
+    elem.requestFullscreen();
+    event.target.value = "";
+
+  }
+
+  if(newValue === "delallcookie"){
+
+    document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    document.cookie = "shortcut_5_stuff=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    document.cookie = "shortcut_4_stuff=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    document.cookie = "shortcut_3_stuff=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    document.cookie = "shortcut_2_stuff=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    document.cookie = "shortcut_1_stuff=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+
+    event.target.value = "";
+
+  }
+
+}
+
+textField1.addEventListener("keypress", Text);
+textField2.addEventListener("keypress", Text);
 
 });
 
+document.getElementById("FS_remove_id").addEventListener("click", function(){
 
+var FS = document.querySelector(".FS");
+var container = document.querySelector(".container")
 
+container.style.display = ""
+FS.style.display = "none";
+
+});
+
+document.getElementById("FS_form_id").addEventListener("submit", function(event){
+  
+  var cname = document.getElementById("test_text_id").value;
+
+  event.preventDefault();
+  setCookie("username", cname, 365);
+  location.reload();
+
+  });
+
+document.getElementById("FullS_id").addEventListener("click",function(){
+
+  var elem = document.documentElement;
+
+  if(elem.requestFullscreen){
+    
+    elem.requestFullscreen();
+
+  }
+})
