9992b13927335599dd60678385c596cf153299c5
run-Nexus21 <run-Nexus21@gmail.com>
2024-02-27 09:44:03 +0100

    bug fix and stuff idk

--- stuff.js
+++ stuff.js
@@ -154,23 +154,44 @@ document.addEventListener("DOMContentLoaded", function (){
   var element = document.getElementById(("HowTFDoIMakeTime_id"));
   
   if(true == true){
-    if(element.innerText >= "00:00:00" && element.innerText <= "12:59:59"){
+    if(element.innerText >= "00:00:00" && element.innerText <= "11:59:59"){
   
     document.cookie = "what_time=; expires=1 jan 1999 00:00:00";
     setCookie("what_time", "Good morning, ", 365);
+    let username = getCookie("username");
+    let what_time = getCookie("what_time");
+
+    document.getElementById("ms_user").textContent = (what_time + " " + username);
+    document.getElementById("wallpaper_user_id").textContent = (what_time + " " + username);
+    document.getElementById("first_text2_id").textContent = (what_time + " Sir. Shall we?")
+
   }
   
   if(element.innerText >= "12:00:00" && element.innerText <= "17:59:59"){
   
     document.cookie = "what_time=; expires=1 jan 1999 00:00:00";
     setCookie("what_time", "Good afternoon, ", 365);
-  
+    let username = getCookie("username");
+    let what_time = getCookie("what_time");
+
+    document.getElementById("ms_user").textContent = (what_time + " " + username);
+    document.getElementById("wallpaper_user_id").textContent = (what_time + " " + username);
+    document.getElementById("first_text2_id").textContent = (what_time + " Sir. Shall we?")
+
   }
   
   if(element.innerText >= "18:00:00" && element.innerText <= "23:59:59"){
   
     document.cookie = "what_time=; expires=1 jan 1999 00:00:00";
     setCookie("what_time", "Good evening, ", 365)
+
+    let username = getCookie("username");
+    let what_time = getCookie("what_time");
+
+    document.getElementById("ms_user").textContent = (what_time + " " + username);
+    document.getElementById("wallpaper_user_id").textContent = (what_time + " " + username);
+    document.getElementById("first_text2_id").textContent = (what_time + " Sir. Shall we?")
+
   }
   }
   });
@@ -349,13 +370,13 @@ document.getElementById("text_outline_inout_id").addEventListener("keypress", fu
     what_color_outline = "2px " + document.getElementById("text_outline_inout_id").value
     setCookie("outline_color", what_color_outline, 365)
 
-    document.querySelector(".messagesandtime").style.webkitTextStroke = what_color_outline;
+    document.querySelector(".messagesandtime_ol").style.webkitTextStroke = what_color_outline;
     
   }
 });
 
   what_color_outline = getCookie("outline_color");
-  document.querySelector(".messagesandtime").style.webkitTextStroke = what_color_outline;
+  document.querySelector(".messagesandtime_ol").style.webkitTextStroke = what_color_outline;
 
 //boxshadow
 
@@ -396,12 +417,12 @@ document.getElementById("text_font_shadow_id").addEventListener("keypress", func
 
   setCookie("color_font_shadow", what_font_shadow, 365);
 
-  document.querySelector(".messagesandtime").style.textShadow = what_font_shadow;
+  document.querySelector(".messagesandtime_ol").style.textShadow = what_font_shadow;
 });
 
 var what_font_shadow = getCookie("color_font_shadow")
 
-document.querySelector(".messagesandtime").style.textShadow = what_font_shadow;
+document.querySelector(".messagesandtime_ol").style.textShadow = what_font_shadow;
 
 // borderlinecolor
 
@@ -791,7 +812,8 @@ if(document.getElementById("codefield_text_id").addEventListener("keypress", fun
     document.cookie = "border_color=; expires=1 jan 1999 00:00:00"
     document.cookie = "what_time=; expires= 1 jan 2000 00:00:00"
     document.cookie = "font_style=; expires= 1 jan 2000 00:00:00"
-
+    document.cookie = "border_radius=; expires= 1 jan 2000 00:00:00"
+    
     location.reload()
     event.target.value = "";
 
